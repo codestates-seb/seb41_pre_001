4e4518bfe43139bdbb8419145f1928fe0872e0ea
dlawnsrjs@gmail.com <dlawnsrjs@gmail.com>
2022-12-20 16:30:47 +0900

    feat: BE_Develop_Comment 이름변경

--- src/main/java/com/seb/seb41_preproject/Seb41PreprojectApplication.java
+++ src/main/java/com/seb/seb41_preproject/Seb41PreprojectApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class Seb41PreprojectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Seb41PreprojectApplication.class, args);
     }
 
