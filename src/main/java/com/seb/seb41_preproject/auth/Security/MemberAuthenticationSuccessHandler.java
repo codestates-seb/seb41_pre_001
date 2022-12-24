5c3e5ea2c0567cf4132a3f4d6eab2d46d0b10763
dlawnsrjs@gmail.com <dlawnsrjs@gmail.com>
2022-12-24 19:48:35 +0900

    feat: jwt 구현
    # access, refresh 토큰 구현
    # 요청 데이터 파싱 후 jwt 검증로직 구현

---
+++ src/main/java/com/seb/seb41_preproject/auth/Security/MemberAuthenticationSuccessHandler.java
@@ -0,0 +1,22 @@
+package com.seb.seb41_preproject.auth.Security;
+
+import com.seb.seb41_preproject.member.entity.Member;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class MemberAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        log.info("# Authenticated successfully!");
+    }
+}
--- src/main/java/com/seb/seb41_preproject/member/service/MemberService.java
+++ src/main/java/com/seb/seb41_preproject/member/service/MemberService.java
@@ -1,26 +1,45 @@
 package com.seb.seb41_preproject.member.service;
 
+import com.seb.seb41_preproject.auth.MemberAuthorityUtils;
 import com.seb.seb41_preproject.exception.BusinessLogicException;
 import com.seb.seb41_preproject.exception.ExceptionCode;
 import com.seb.seb41_preproject.member.entity.Member;
 import com.seb.seb41_preproject.member.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
-
+@Slf4j
+@Transactional
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final MemberAuthorityUtils authorityUtils;
 
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder, MemberAuthorityUtils authorityUtils) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authorityUtils = authorityUtils;
     }
 
-    public Member createMember(Member member) {
-        verifyExistUserEmail(member.getUserEmail());
 
-        Member savedMember = memberRepository.save(member);
-        return savedMember;
+    public Member createMember(Member member) {
+
+        verifyExistUserEmail(member.getUserEmail());
+        log.info("password {}", member.getUserPassword());
+
+        String encryptedPassword = passwordEncoder.encode(member.getUserPassword());
+        member.setUserPassword(encryptedPassword);
+
+
+        List<String> roles = authorityUtils.createRoles(member.getUserEmail());
+        member.setRoles(List.of(roles.toString()));
+
+        return memberRepository.save(member);
     }
 
     private void verifyExistUserEmail(String userEmail) {
@@ -34,8 +53,7 @@ public class MemberService {
 
     private Member verifyExistUserId(Long userId) {
         Optional<Member> optionalMember = memberRepository.findById(userId);
-        Member findMember = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
-        return findMember;
+        return optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
     }
 
     public void deleteMember(Long userId) {
