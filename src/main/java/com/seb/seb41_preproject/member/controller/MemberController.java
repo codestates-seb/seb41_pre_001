f9a6caf95818d04d54b70cd53ba3aa84acc8cd6e
dlawnsrjs@gmail.com <dlawnsrjs@gmail.com>
2022-12-27 14:26:33 +0900

    feat: jwt

--- src/main/java/com/seb/seb41_preproject/member/controller/MemberController.java
+++ src/main/java/com/seb/seb41_preproject/member/controller/MemberController.java
@@ -1,27 +1,30 @@
 package com.seb.seb41_preproject.member.controller;
 
+import com.seb.seb41_preproject.Security.Dto.MemberLoginRequestDto;
+import com.seb.seb41_preproject.Security.Info.TokenInfo;
 import com.seb.seb41_preproject.member.entity.Member;
 import com.seb.seb41_preproject.member.mapper.MemberMapper;
 import com.seb.seb41_preproject.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import static com.seb.seb41_preproject.member.dto.MemberDto.*;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class MemberController {
 
     private final MemberMapper memberMapper;
     private final MemberService memberService;
-
-    public MemberController(MemberMapper memberMapper, MemberService memberService) {
-        this.memberMapper = memberMapper;
-        this.memberService = memberService;
-    }
 
     @GetMapping("/{user_id}")
     public ResponseEntity getUserInfo(@PathVariable("user_id") Long userId) {
@@ -56,6 +59,14 @@ public class MemberController {
                 ## 멤버 탈퇴 완료
                 =====================""");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/login")
+    public TokenInfo login(@RequestBody MemberLoginRequestDto memberLoginRequestDto) {
+        String memberId = memberLoginRequestDto.getMemberId();
+        String password = memberLoginRequestDto.getPassword();
+        TokenInfo tokenInfo = memberService.login(memberId, password);
+        return tokenInfo;
     }
 
 }
--- src/main/java/com/seb/seb41_preproject/member/service/MemberService.java
+++ src/main/java/com/seb/seb41_preproject/member/service/MemberService.java
@@ -1,14 +1,17 @@
 package com.seb.seb41_preproject.member.service;
 
-import com.seb.seb41_preproject.auth.MemberAuthorityUtils;
+import com.seb.seb41_preproject.Security.Info.TokenInfo;
+import com.seb.seb41_preproject.Security.Provider.JwtTokenProvider;
 import com.seb.seb41_preproject.exception.BusinessLogicException;
 import com.seb.seb41_preproject.exception.ExceptionCode;
+import com.seb.seb41_preproject.member.controller.MemberAuthorityUtils;
 import com.seb.seb41_preproject.member.entity.Member;
 import com.seb.seb41_preproject.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,18 +19,16 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 @Slf4j
-@Transactional
+@Transactional(readOnly = true)
 @Service
+@RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final MemberAuthorityUtils authorityUtils;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder, MemberAuthorityUtils authorityUtils) {
-        this.memberRepository = memberRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.authorityUtils = authorityUtils;
-    }
 
 
     public Member createMember(Member member) {
@@ -61,5 +62,20 @@ public class MemberService {
         Optional<Member> optionalMember = memberRepository.findById(userId);
         Member findMember = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
         memberRepository.delete(findMember);
+    }
+    @Transactional
+    public TokenInfo login(String memberId, String password) {
+        // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
+        // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(memberId, password);
+
+        // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
+        // authenticate 매서드가 실행될 때 CustomUserDetailsService 에서 만든 loadUserByUsername 메서드가 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
+
+        return tokenInfo;
     }
 }
