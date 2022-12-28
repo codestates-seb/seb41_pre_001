24aa35d481566fbc3b9a6aa090a49ab26a898177
ty <sycork@gamil.com>
2022-12-28 17:33:24 +0900

    수정

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import QuestionList from './pages/QuestionList';
 // import main from './main.js';
 import Tags from './pages/Tags';
 import User from './pages/User';
@@ -15,6 +14,7 @@ import ExploreCollectives from './pages/ExploreCollectives';
 import QuestionCreate from './pages/QuestionCreate';
 import { Route, Routes } from 'react-router-dom';
 import ReactModal from 'react-modal';
+import QuestionList from './pages/QuestionList';
 
 const BodyContainer = styled.div`
   padding-top: 53px;
